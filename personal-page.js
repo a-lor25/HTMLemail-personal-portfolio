0cf44e4fe252a70885d32b89b7c70eaa97dd68db
a-lor25 <alor54901@gmail.com>
2025-03-01 11:52:23 -0600

    Add files via upload

---
+++ personal-page.js
@@ -0,0 +1,28 @@
+const toggleBtn = document.querySelector('.toggle-btn')
+const toggleBtnIcon = document.querySelector('.toggle-btn i')
+const dropDownMenu = document.querySelector('.dropdown-menu')
+
+toggleBtn.onclick = function () {
+  dropDownMenu.classList.toggle('open')
+}
+
+/*toggleBtnIcon.addEventListener("mouseout", event => {
+  dropDownMenu.style.display = "none";
+})*/
+
+/*document.getElementById("emailForm").addEventListener("submit", async function(event) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+
+  const response = await fetch("https://your-backend-api.com/send-email", {
+    method: "POST",
+    body: JSON.stringify(Object.fromEntries(formData)),
+    headers: { "Content-Type": "application/json" }
+  });
+
+  if (response.ok) {
+    alert("Email sent successfully!");
+  } else {
+    alert("Failed to send email.");
+  }
+});*/
